5932f0ba5c3cb1b589f944529f8906023b1e3745
kiran <varmakiran190@gmail.com>
2026-04-18 17:54:44 +0530

    updated Hello successfully

--- src/main/java/com/example/devopsTraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopsTraining/DevopsTrainingApplication.java
@@ -8,11 +8,8 @@ public class DevopsTrainingApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
-<<<<<<< HEAD
 		System.out.println("hello mingey");
-=======
 		System.out.println("hello");
->>>>>>> 59c3542 (updated Hello)
 		SpringApplication.run(DevopsTrainingApplication.class, args);
 	}
 
